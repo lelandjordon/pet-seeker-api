837024529cc9b90dbd17fc8d1b098f785ebfe195
Leland Jordon <lelandjordon@mac.com>
2016-06-14 19:54:15 -0400

    added pets to second shelter in seed data

--- db/pet_data.rb
+++ db/pet_data.rb
@@ -13,7 +13,20 @@ def get_pet_data
               :age =>        2,
               :name =>       "Puffy",
               :photo_url =>  "http://25.media.tumblr.com/tumblr_m2e2300cak1qhz0g3o1_500.jpg"
-            }]
+            }],
+    "happyhouse" => [{
+              :species =>    "Dog",
+              :breed =>      "Boxer",
+              :age =>        5,
+              :name =>       "Stinky",
+              :photo_url =>  "http://3.bp.blogspot.com/_LTWqGVqO5uQ/TNkA9qzVTSI/AAAAAAAAAR8/gLE_gv9hAFc/s1600/Boxer3523.jpg"
+            },
+              :species =>    "Horse",
+              :breed =>      "Draft Horse",
+              :age =>        2,
+              :name =>       "Fragrant",
+              :photo_url =>  "http://www.worlddrafthorsenetwork.com/site/content/images/sale/49_1.jpg"
+      }]
   }
   return pet_data
 end
